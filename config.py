992a88c829dfb6c2972dbf3d032990165cc9a8e7
senel34 <joshuapohan93@gmail.com>
2018-06-04 22:21:17 +0700

    added pagination

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config(object):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir,'nblog.db')
 	SQLALCHEMY_TRACK_MODIFICATION = False
+	POSTS_PER_PAGE = 3
